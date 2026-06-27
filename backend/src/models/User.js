63adfa2d7009421d80fa91f1e2cdf4c652534154
Prathamesh <prathampatil123789@gmail.com>
2026-06-27 14:26:42 +0530

    manages container using kubernetes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -26,18 +26,12 @@ const userSchema = new mongoose.Schema({
     }
 );
 
-// Fixed: Added 'function' keyword and 'next' parameter. 
-// Arrow functions do not bind 'this', so 'function' is required here.
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// Use a normal function so Mongoose binds `this` to the document.
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Fixed: Removed 'name' from function definition syntax
